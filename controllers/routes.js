a663d0007894eb677f9ac181315dc7713bed9faa
Jason Whited <jsn.whited@gmail.com>
2019-12-09 22:51:11 -0500

    added more files

---
+++ controllers/routes.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/',function(req,res){
+    burger.all(function(req,res){
+        console.log(burger_data);
+        res.render('index');
+    })
+    
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
 var app = express();
-app.use(express.static(_dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -16,5 +16,11 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+var routes = require('./controllers/routes.js');
+app.use('/',routes);
+
 var port = 3000;
-app.listen(port)
+app.listen(port, function (){
+    console.log("Server is listening on: http//localhost:" + port);
+});
+
